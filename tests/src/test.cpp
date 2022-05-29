51931e1f460f62086f2ca80c00cae6790f7f1d23
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-29 10:10:23 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,22 +1,27 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/shell.hpp>
 
-TEST_CASE("make_background_command_string") {
-  {
-    std::string command = "open '/Applications/Utilities/Activity Monitor.app'";
-    std::string expected = "open '/Applications/Utilities/Activity Monitor.app' &";
-    REQUIRE(pqrs::shell::make_background_command_string(command) == expected);
-  }
-  {
-    std::string command = "open '/Applications/Utilities/Activity Monitor.app'&";
-    std::string expected = "open '/Applications/Utilities/Activity Monitor.app'&";
-    REQUIRE(pqrs::shell::make_background_command_string(command) == expected);
-  }
-  {
-    std::string command = "ls & echo hello";
-    std::string expected = "ls & echo hello &";
-    REQUIRE(pqrs::shell::make_background_command_string(command) == expected);
-  }
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
+
+  "make_background_command_string"_test = [] {
+    {
+      std::string command = "open '/Applications/Utilities/Activity Monitor.app'";
+      std::string expected = "open '/Applications/Utilities/Activity Monitor.app' &";
+      expect(pqrs::shell::make_background_command_string(command) == expected);
+    }
+    {
+      std::string command = "open '/Applications/Utilities/Activity Monitor.app'&";
+      std::string expected = "open '/Applications/Utilities/Activity Monitor.app'&";
+      expect(pqrs::shell::make_background_command_string(command) == expected);
+    }
+    {
+      std::string command = "ls & echo hello";
+      std::string expected = "ls & echo hello &";
+      expect(pqrs::shell::make_background_command_string(command) == expected);
+    }
+  };
+
+  return 0;
 }
